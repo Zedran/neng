46cd70036338074f8cc87b0ac97bccc1300c55e7
Zedran <17592740+Zedran@users.noreply.github.com>
2024-05-04 23:48:46 +0200

    verb.go: order functions alphabetically

--- verb.go
+++ verb.go
@@ -2,6 +2,51 @@ package neng
 
 import "strings"
 
+/* Returns gerund form of a verb. */
+func gerund(verb string) string {
+	if len(verb) <= 2 {
+		return verb + "ing"
+	}
+
+	wi := getWordInfo(verb)
+
+	if strings.HasSuffix(verb, "it") {
+		return handleIt(verb, "ing", wi)
+	}
+
+	if strings.HasSuffix(verb, "e") && verb != "ante" {
+		if wi.sequence[len(wi.sequence)-2] == 'c' && verb[len(verb)-2] != 'y' {
+			// Remove final 'e' if previous letter is consonant other than 'y'
+			return verb[:len(verb)-1] + "ing"
+		}
+
+		switch verb[len(verb)-2] {
+		case 'u': // ue
+			return verb[:len(verb)-1] + "ing"
+		case 'i': // ie
+			return verb[:len(verb)-2] + "ying"
+		}
+	}
+
+	if endsWithAny(verb, []string{"h", "s", "w", "x", "y"}) {
+		if strings.HasSuffix(verb, "gas") {
+			// Double the ending of 'gas' and its derivatives
+			return doubleFinal(verb, "ing")
+		}
+
+		return verb + "ing"
+	}
+
+	if strings.HasSuffix(wi.sequence, "cvc") {
+		return handleCVC(verb, "ing", wi, []string{
+			"abet", "abhor", "anagram", "beget", "beset", "curvet", "forget", "inset", "offset", "overrun",
+			"regret", "reset", "sublet", "typeset", "underrun", "upset",
+		})
+	}
+
+	return verb + "ing"
+}
+
 /*
 Handles past tense and gerund transformations for verbs ending with consonant-vowel-consonant sequence. Takes a number of arguments:
 
@@ -67,51 +112,6 @@ func handleIt(verb, tenseEnding string, wi wordInfo) string {
 	return verb + tenseEnding
 }
 
-/* Returns gerund form of a verb. */
-func gerund(verb string) string {
-	if len(verb) <= 2 {
-		return verb + "ing"
-	}
-
-	wi := getWordInfo(verb)
-
-	if strings.HasSuffix(verb, "it") {
-		return handleIt(verb, "ing", wi)
-	}
-
-	if strings.HasSuffix(verb, "e") && verb != "ante" {
-		if wi.sequence[len(wi.sequence)-2] == 'c' && verb[len(verb)-2] != 'y' {
-			// Remove final 'e' if previous letter is consonant other than 'y'
-			return verb[:len(verb)-1] + "ing"
-		}
-
-		switch verb[len(verb)-2] {
-		case 'u': // ue
-			return verb[:len(verb)-1] + "ing"
-		case 'i': // ie
-			return verb[:len(verb)-2] + "ying"
-		}
-	}
-
-	if endsWithAny(verb, []string{"h", "s", "w", "x", "y"}) {
-		if strings.HasSuffix(verb, "gas") {
-			// Double the ending of 'gas' and its derivatives
-			return doubleFinal(verb, "ing")
-		}
-
-		return verb + "ing"
-	}
-
-	if strings.HasSuffix(wi.sequence, "cvc") {
-		return handleCVC(verb, "ing", wi, []string{
-			"abet", "abhor", "anagram", "beget", "beset", "curvet", "forget", "inset", "offset", "overrun",
-			"regret", "reset", "sublet", "typeset", "underrun", "upset",
-		})
-	}
-
-	return verb + "ing"
-}
-
 /* Returns Past Participle form of a verb. */
 func pastParticiple(verb string, verbsIrr [][]string) string {
 	verbLine := findIrregular(verb, verbsIrr)
@@ -120,45 +120,6 @@ func pastParticiple(verb string, verbsIrr [][]string) string {
 	}
 
 	return verbLine[2]
-}
-
-/* Returns Past Simple form of a verb. */
-func pastSimple(verb string, verbsIrr [][]string) string {
-	verbLine := findIrregular(verb, verbsIrr)
-	if verbLine == nil {
-		return pastRegular(verb)
-	}
-
-	return verbLine[1]
-}
-
-/* Returns Present Simple form of a verb. */
-func presentSimple(verb string, plural bool) string {
-	if plural {
-		if verb == "be" {
-			return "are"
-		}
-		return verb
-	}
-
-	switch verb {
-	case "be":
-		return "is"
-	case "have":
-		return "has"
-	}
-
-	seq := getSequence(verb)
-
-	if strings.HasSuffix(verb, "o") && strings.HasSuffix(seq, "cv") {
-		return verb + "es"
-	} else if strings.HasSuffix(verb, "y") && strings.HasSuffix(seq, "v") {
-		return verb[:len(verb)-1] + "ies"
-	} else if endsWithAny(verb, []string{"ch", "s", "sh", "x", "z"}) {
-		return verb + "es"
-	}
-
-	return verb + "s"
 }
 
 /* Appends past tense suffix to a regular verb. */
@@ -201,4 +162,43 @@ func pastRegular(verb string) string {
 	}
 
 	return verb + "ed"
+}
+
+/* Returns Past Simple form of a verb. */
+func pastSimple(verb string, verbsIrr [][]string) string {
+	verbLine := findIrregular(verb, verbsIrr)
+	if verbLine == nil {
+		return pastRegular(verb)
+	}
+
+	return verbLine[1]
+}
+
+/* Returns Present Simple form of a verb. */
+func presentSimple(verb string, plural bool) string {
+	if plural {
+		if verb == "be" {
+			return "are"
+		}
+		return verb
+	}
+
+	switch verb {
+	case "be":
+		return "is"
+	case "have":
+		return "has"
+	}
+
+	seq := getSequence(verb)
+
+	if strings.HasSuffix(verb, "o") && strings.HasSuffix(seq, "cv") {
+		return verb + "es"
+	} else if strings.HasSuffix(verb, "y") && strings.HasSuffix(seq, "v") {
+		return verb[:len(verb)-1] + "ies"
+	} else if endsWithAny(verb, []string{"ch", "s", "sh", "x", "z"}) {
+		return verb + "es"
+	}
+
+	return verb + "s"
 }
