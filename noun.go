41d11a8a020e661891b05344a651573aaa6e72d6
Zedran <17592740+Zedran@users.noreply.github.com>
2024-06-10 22:04:46 +0200

    plural: reorder checks to prioritize more common suffixes

--- noun.go
+++ noun.go
@@ -9,42 +9,42 @@ func plural(noun string, nounsIrr [][]string) string {
 		return nounLine[1]
 	}
 
-	if endsWithAny(noun, []string{"craft", "fish", "gs", "hertz", "ics"}) {
+	if endsWithAny(noun, []string{"fish", "ics", "gs", "craft", "hertz"}) {
 		return noun
+	}
+
+	if endsWithAny(noun, []string{"sh", "ch", "fix", "virus"}) {
+		return noun + "es"
 	}
 
 	if endsWithAny(noun, []string{"ff", "io", "tto"}) {
 		return noun + "s"
 	}
 
-	if endsWithAny(noun, []string{"ch", "fix", "sh", "virus"}) {
-		return noun + "es"
-	}
-
 	switch noun[len(noun)-2:] {
-	case "ex", "ix":
-		return noun[:len(noun)-2] + "ices"
-	case "fe":
-		return noun[:len(noun)-2] + "ves"
-	case "is":
-		return noun[:len(noun)-2] + "es"
 	case "um":
 		return noun[:len(noun)-2] + "a"
 	case "us":
 		return noun[:len(noun)-2] + "i"
+	case "is":
+		return noun[:len(noun)-2] + "es"
+	case "ex", "ix":
+		return noun[:len(noun)-2] + "ices"
+	case "fe":
+		return noun[:len(noun)-2] + "ves"
 	}
 
 	seq := getSequence(noun)
 
 	switch noun[len(noun)-1] {
-	case 'f':
-		return noun[:len(noun)-1] + "ves"
-	case 'o', 's', 'x':
-		return noun + "es"
 	case 'y':
 		if strings.HasSuffix(seq, "v") {
 			return noun[:len(noun)-1] + "ies"
 		}
+	case 's', 'o', 'x':
+		return noun + "es"
+	case 'f':
+		return noun[:len(noun)-1] + "ves"
 	case 'z':
 		if strings.HasSuffix(seq, "vc") {
 			return doubleFinal(noun, "es")
