6625a6b3bd5542688a93e2f68d192a690b4449e0
Zedran <17592740+Zedran@users.noreply.github.com>
2024-07-18 15:48:17 +0200

    Phrase example: introductory message is now a const

--- examples/phrase/main.go
+++ examples/phrase/main.go
@@ -9,6 +9,15 @@ import (
 	"github.com/Zedran/neng"
 )
 
+const HELP string = "Specify your phrase.\n\n" +
+	"a - adjective         m - adverb\n" +
+	"n - noun              v - verb\n\n" +
+	"2 - Past Simple       3 - Past Participle    N - Present Simple\n" +
+	"c - Comparative       g - Gerund             p - Plural\n" +
+	"l - lower case        s - Superlative        t - Title Case\n" +
+	"u - UPPER CASE\n\n" +
+	"Example: %tsa %tpn that %m %Npv the %n\n\n"
+
 func main() {
 	gen, err := neng.DefaultGenerator()
 	if err != nil {
@@ -16,21 +25,12 @@ func main() {
 	}
 
 	var (
-		help string = "Specify your phrase.\n\n" +
-			"a - adjective         m - adverb\n" +
-			"n - noun              v - verb\n\n" +
-			"2 - Past Simple       3 - Past Participle    N - Present Simple\n" +
-			"c - Comparative       g - Gerund             p - Plural\n" +
-			"l - lower case        s - Superlative        t - Title Case\n" +
-			"u - UPPER CASE\n\n" +
-			"Example: %tsa %tpn that %m %Npv the %n\n\n"
-
 		pattern string
 		phrase  string
 		scanner = bufio.NewScanner(os.Stdin)
 	)
 
-	fmt.Print(help)
+	fmt.Printf("%s", HELP)
 
 	for {
 		fmt.Print("pattern> ")
