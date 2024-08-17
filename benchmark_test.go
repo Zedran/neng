0e2c28fdf19027246120cc5fe74729b61e6914fa
Zedran <17592740+Zedran@users.noreply.github.com>
2024-08-17 18:02:56 +0200

    Add benchmarks

---
+++ benchmark_test.go
@@ -0,0 +1,178 @@
+package neng
+
+import "testing"
+
+func BenchmarkDefaultGenerator(b *testing.B) {
+	for range b.N {
+		DefaultGenerator()
+	}
+}
+
+func BenchmarkGenerator_Phrase(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+
+	b.StartTimer()
+	for range b.N {
+		gen.Phrase("%tsa %tpn that %m %Npv the %n")
+	}
+}
+
+func BenchmarkTransformAll_Adj(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+
+	list := make([]string, len(gen.adj))
+	for i, w := range gen.adj {
+		list[i] = w.word
+	}
+
+	wc := WC_ADJECTIVE
+
+	b.StartTimer()
+	for range b.N {
+		for _, w := range list {
+			f, _ := gen.Find(w, wc)
+
+			gen.TransformWord(f, wc, MOD_COMPARATIVE)
+			gen.TransformWord(f, wc, MOD_SUPERLATIVE)
+		}
+	}
+}
+
+func BenchmarkTransformAll_Adv(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+
+	list := make([]string, len(gen.adv))
+	for i, w := range gen.adv {
+		list[i] = w.word
+	}
+
+	wc := WC_ADVERB
+
+	b.StartTimer()
+	for range b.N {
+		for _, w := range list {
+			f, _ := gen.Find(w, wc)
+
+			gen.TransformWord(f, wc, MOD_COMPARATIVE)
+			gen.TransformWord(f, wc, MOD_SUPERLATIVE)
+		}
+	}
+}
+
+func BenchmarkTransformAll_Noun(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+
+	list := make([]string, len(gen.noun))
+	for i, w := range gen.noun {
+		list[i] = w.word
+	}
+
+	wc := WC_NOUN
+
+	b.StartTimer()
+	for range b.N {
+		for _, w := range list {
+			f, _ := gen.Find(w, wc)
+
+			gen.TransformWord(f, wc, MOD_PLURAL)
+		}
+	}
+}
+
+func BenchmarkTransformAll_Verb(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+
+	list := make([]string, len(gen.verb))
+	for i, w := range gen.verb {
+		list[i] = w.word
+	}
+
+	wc := WC_VERB
+
+	b.StartTimer()
+	for range b.N {
+		for _, w := range list {
+			f, _ := gen.Find(w, wc)
+
+			gen.TransformWord(f, wc, MOD_PAST_SIMPLE)
+			gen.TransformWord(f, wc, MOD_PAST_PARTICIPLE)
+			gen.TransformWord(f, wc, MOD_PRESENT_SIMPLE)
+			gen.TransformWord(f, wc, MOD_GERUND)
+		}
+	}
+}
+
+func BenchmarkTransformOne_Adj(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+	w := "little"
+	wc := WC_ADJECTIVE
+
+	b.StartTimer()
+	for range b.N {
+		f, _ := gen.Find(w, wc)
+
+		gen.TransformWord(f, wc, MOD_COMPARATIVE)
+		gen.TransformWord(f, wc, MOD_SUPERLATIVE)
+	}
+}
+
+func BenchmarkTransformOne_Adv(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+	w := "far"
+	wc := WC_ADVERB
+
+	b.StartTimer()
+	for range b.N {
+		f, _ := gen.Find(w, wc)
+
+		gen.TransformWord(f, wc, MOD_COMPARATIVE)
+		gen.TransformWord(f, wc, MOD_SUPERLATIVE)
+	}
+}
+
+func BenchmarkTransformOne_Noun(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+	w := "modulus"
+	wc := WC_NOUN
+
+	b.StartTimer()
+	for range b.N {
+		f, _ := gen.Find(w, wc)
+
+		gen.TransformWord(f, wc, MOD_PLURAL)
+	}
+}
+
+func BenchmarkTransformOne_Verb(b *testing.B) {
+	b.StopTimer()
+
+	gen, _ := DefaultGenerator()
+	w := "overcome"
+	wc := WC_VERB
+
+	b.StartTimer()
+	for range b.N {
+		f, _ := gen.Find(w, wc)
+
+		gen.TransformWord(f, wc, MOD_PAST_SIMPLE)
+		gen.TransformWord(f, wc, MOD_PAST_PARTICIPLE)
+		gen.TransformWord(f, wc, MOD_PRESENT_SIMPLE)
+		gen.TransformWord(f, wc, MOD_GERUND)
+	}
+}
