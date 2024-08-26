42ee9dd54035b38340a676504d2a7478c9b4fcc5
Zedran <17592740+Zedran@users.noreply.github.com>
2024-08-26 17:35:27 +0200

    Make test_data_directory value local to loadTestData function

--- helpers_test.go
+++ helpers_test.go
@@ -6,12 +6,9 @@ import (
 	"path/filepath"
 )
 
-// Default test data directory
-const test_data_directory string = "test_data"
-
 /* Unmarshals JSON file to an arbitrary data structure. */
 func loadTestData(fname string, v any) error {
-	stream, err := os.ReadFile(filepath.Join(test_data_directory, fname))
+	stream, err := os.ReadFile(filepath.Join("test_data", fname))
 	if err != nil {
 		return err
 	}
