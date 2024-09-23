adc26885be697dd0a193da7d704f4306bdfc6457
Zedran <17592740+Zedran@users.noreply.github.com>
2024-09-23 23:53:41 +0200

    Add the prototype indefinite function

---
+++ indefinite.go
@@ -0,0 +1,36 @@
+package neng
+
+import "strings"
+
+// indefinite returns the word prefixed with an indefinite article.
+func indefinite(word string) string {
+	switch word[0] {
+	case 'a', 'i', 'e', 'o':
+		return "an " + word
+	case 'u':
+		return indefiniteU(word)
+	}
+	return "a " + word
+}
+
+// indefiniteU prefixes the word beginning with 'u-' with an indefinite article.
+func indefiniteU(word string) string {
+	switch word[1] {
+	case 'n':
+		if strings.HasPrefix(word, "uni") {
+			if !strings.ContainsRune("nmdr", rune(word[3])) || word == "unimodal" || word == "uninominal" {
+				// unin-, unim-, unid-, unir- (for a single 'unironed')
+				return "a " + word
+			}
+		}
+	case 's':
+		if !strings.HasPrefix(word, "ush") {
+			return "a " + word
+		}
+	case 't':
+		if !strings.HasPrefix(word, "utt") {
+			return "a " + word
+		}
+	}
+	return "an " + word
+}
